9ec79490638dc61a7b39f1c03d1be842c8712ef0
Sohan Dutta <sohan.dutta11@gmail.com>
2025-03-11 00:04:08 +0530

    feat: add tanstack query devtools

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,7 @@
 import Router from "./Router";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 2, refetchOnWindowFocus: false } }
@@ -12,6 +13,8 @@ function App() {
       <MantineProvider>
         <Router />
       </MantineProvider>
+
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
